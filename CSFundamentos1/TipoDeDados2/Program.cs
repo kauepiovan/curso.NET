2e4b5cc7bc8362d15cb40d0c943cdf209adee043
Kauê Piovan <kaupiovan@gmail.com>
2024-01-04 22:18:48 -0300

    tipos numericos

---
+++ CSFundamentos1/TipoDeDados2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("atribuindo valores a double, float e decimal");
+Console.WriteLine();
+double n1 = 1.234;
+float n2 = 1.234f;
+decimal n3 = 1.234m;
+
+Console.WriteLine(n1);
+Console.WriteLine(n2);
+Console.WriteLine(n3);
+
+float x = 1f / 3f;
+double y = 1d / 3d;
+decimal z = 1m / 3m;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
---
+++ CSFundamentos1/TiposDeDados1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Declarando variaveis");
+Console.WriteLine();
+// declarando variaveis
+byte valor1 = 255;
+sbyte valor2 = -127;
+int valor3 = -2147483647;
+uint valor4 = 2147483647;
+long valor5 = -21474836489;
+short valor7 = 32767;
+
+const int valor6 = 100;
+
+int var1 = 8, var2 = 9, var3 = 10;
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+Console.WriteLine(valor4);
+Console.WriteLine(valor5);
+Console.WriteLine(valor6);
+Console.WriteLine(valor7);
+Console.WriteLine(var1);
+Console.WriteLine(var2);
+Console.WriteLine(var3);
+
+Console.ReadLine();
