1b10dd835f250923014bb6ac8ce55ce52a17911e
Kauê Piovan <kaupiovan@gmail.com>
2024-01-15 17:52:24 -0300

    .

---
+++ CSFundamentos1/EstruturaCondicional/Program.cs
@@ -0,0 +1,30 @@
+﻿// estruturas condicionais 
+
+Console.WriteLine("é um cliente especial? (s/n)");
+var isEspecialClient = Console.ReadLine();
+
+if (isEspecialClient == "s")
+{
+    Console.WriteLine("Desconto de 10% aplicado");
+}
+
+Console.WriteLine("fim do programa");
+
+Console.WriteLine("digite o numero de x");
+var x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("digite o numero de y");
+var y = Convert.ToInt32(Console.ReadLine());
+
+if (x > y) 
+{
+    Console.WriteLine("x é maior que y");
+}
+if (x < y) 
+{
+    Console.WriteLine("x é menor que y");
+}
+if (x == y)
+{
+    Console.WriteLine("x é igual a y");
+}
---
+++ CSFundamentos1/EstruturaCondicional1/Program.cs
@@ -0,0 +1,34 @@
+﻿// estruturas condicionais
+
+Console.WriteLine("informe a nota do aluno");
+var notaAluno = Convert.ToInt32(Console.ReadLine());
+
+if (notaAluno > 5)
+{
+    Console.WriteLine("Aluno Aprovado");
+}
+else
+{
+    Console.WriteLine("Aluno Reprovado");
+}
+
+Console.WriteLine("Informe o valor de x");
+var x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("informe o valor de y");
+var y = Convert.ToInt32(Console.ReadLine());
+
+if (x > y)
+{
+    Console.WriteLine("X é maior que Y");
+}
+else
+{
+    if (x < y)
+    {
+        Console.WriteLine("X é menor que Y");
+    }
+    else 
+    {
+        Console.WriteLine("X é igual a Y");
+    }
+}
