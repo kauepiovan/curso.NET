8a5de46808e8d788a84d76bee7f4a80a078c5687
Kauê Piovan <kaupiovan@gmail.com>
2024-01-18 14:20:27 -0300

    .

---
+++ Exercicios/Ex005/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Qual a instrução para sair de um loop?\na)quit\nb)continue\nc)break\nd)exit");
+var respostaCorreta = 'c';
+
+while (true)
+{
+    Console.WriteLine("Qual a resposta correta? (tecle X para sair) ");
+    char respostaUsuario = Convert.ToChar(Console.ReadLine());
+
+    if (respostaUsuario == 'x' || respostaUsuario == 'X')
+    {
+        break;
+    }
+
+    if (respostaUsuario == respostaCorreta)
+    {
+        Console.WriteLine("Resposta Correta");
+        break;
+    }
+    
+    Console.WriteLine("Resposta Incorreta.");
+}
+Console.WriteLine("programa finalizado");
