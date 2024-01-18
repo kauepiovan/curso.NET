22ed3a2bf17ef8372043c59df63f9e0292443ab8
Kauê Piovan <kaupiovan@gmail.com>
2024-01-18 15:20:12 -0300

    .

---
+++ Exercicios/Ex011/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("digite um numero");
+int numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite a operação (+ - / *)");
+char operacao = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("digite outro numero");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+
+switch (operacao)
+{
+    case '+':
+        Console.WriteLine($"{numero1} + {numero2} = {numero1 + numero2}");
+        break;
+    case '-':
+        Console.WriteLine($"{numero1} - {numero2} = {numero1 - numero2}");
+        break;
+    case '*':
+        Console.WriteLine($"{numero1} * {numero2} = {numero1 * numero2}");
+
+        break;
+    case '/':
+        if (numero2 == 0)
+        {
+            Console.WriteLine("Divisão por 0 impossivel");
+            break;
+        }
+        else
+        {
+        Console.WriteLine($"{numero1} / {numero2} = {numero1 / (float)numero2}");
+        break;
+        }
+        
+}
