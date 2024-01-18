177c7e44c67ed0187f808a3f2b96978edaf3be0e
Kauê Piovan <kaupiovan@gmail.com>
2024-01-18 04:18:33 -0300

    .

---
+++ CSFundamentos1/EstruturaRepeticao2/Program.cs
@@ -0,0 +1,36 @@
+﻿// estrutura de repeticao 2
+// estrutura de repeticao for
+
+int resultado, numero;
+
+Console.WriteLine("Informe o numero desejado");
+numero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"tabuada do numero {numero}");
+
+for (int i = 0; i <= 10; i++)
+{
+    resultado = i * numero;
+    Console.WriteLine($"{i} x {numero} = {resultado}");
+}
+
+Console.WriteLine("fim do programa");
+
+for (int i = 0, j = 1, k = 2; k <= 10; i++, j++, k++)
+{
+    Console.WriteLine($"i = {i}, j = {j}, k = {k}");
+}
+
+for (int i = 0; i <= 10; i++)
+{
+    if (i == 5)
+    {
+        break;
+    }
+    if (i == 3)
+    {
+        continue;
+    }
+
+    Console.WriteLine(i);
+}
