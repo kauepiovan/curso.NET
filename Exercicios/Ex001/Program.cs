5dd2a6dd3de1974205f82d3e43016a7cf64dffe8
Kauê Piovan <kaupiovan@gmail.com>
2024-01-18 13:32:51 -0300

    .

---
+++ Exercicios/Ex001/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("exercicio - 01");
+
+int maiorNumero = int.MinValue;
+for (int i = 0; i <= 2; i++)
+{
+    Console.Write("Digite um numero: ");
+    int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+    if (numeroDigitado > maiorNumero)
+    {
+        maiorNumero = numeroDigitado;
+    }
+}
+Console.WriteLine($"O maior numero digitador foi {maiorNumero}");
