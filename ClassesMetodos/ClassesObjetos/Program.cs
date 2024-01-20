3f5518d45b91c59f220a10eeea9a2b973a009f62
Kauê Piovan <kaupiovan@gmail.com>
2024-01-20 01:19:01 -0300

    .

---
+++ ClassesMetodos/ClassesObjetos/Program.cs
@@ -0,0 +1,22 @@
+﻿// Classes e metodos
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "joao";
+p1.Idade = 20;
+p1.Sexo = "Masculino";
+
+Console.WriteLine($"{p1.Nome}, {p1.Idade}, {p1.Sexo}");
+
+Pessoa p2 = new Pessoa();
+p2.Nome = "maria";
+p2.Idade = 22;
+p2.Sexo = "Feminino";
+
+Console.WriteLine($"{p2.Nome}, ${p2.Idade}, {p2.Sexo}");
+
+class Pessoa
+{
+    public string? Nome;
+    public int Idade;
+    public string? Sexo;
+}
