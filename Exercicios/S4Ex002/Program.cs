b10f5041bf2d060f32a9493b6e1ae7f55bbc2185
Kauê Piovan <kaupiovan@gmail.com>
2024-01-22 19:29:01 -0300

    .

---
+++ Exercicios/S4Ex002/Program.cs
@@ -0,0 +1,21 @@
+﻿var carro01 = new Carro("Sedan", "Lancer");
+double potencia = carro01.VelocidadeMaxima(150);
+
+Console.WriteLine(potencia);
+
+public class Carro
+{
+    public string? Modelo;
+    public string? Marca;
+
+    public Carro(string modelo, string marca)
+    {
+        Modelo = modelo;
+        Marca = marca;
+    }
+
+    public double VelocidadeMaxima(int potencia)
+    {
+        return potencia * 1.75;
+    }
+}
