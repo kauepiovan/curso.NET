da871c3d5ef25398dbd035f7d19dd56fb435588c
Kauê Piovan <kaupiovan@gmail.com>
2024-01-25 04:29:16 -0300

    .

---
+++ ClassesMetodos/Parametros/Program.cs
@@ -0,0 +1,15 @@
+﻿var calc = new Calcular();
+
+int x = 20;
+Console.WriteLine($"(antes do metodo) Valor de x = {x}");
+calc.Dobrar(x);
+Console.WriteLine($"(depois do metodo) Valor de x = {x}");
+
+public class Calcular
+{
+    public void Dobrar(int y)
+    {
+        y *= 2;
+        Console.WriteLine($"Valor do parametro y = {y}");
+    }
+}
