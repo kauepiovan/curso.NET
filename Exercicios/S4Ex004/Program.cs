4a5f5f94c62f092f0950b29cb62c8db8d6b9f90b
Kauê Piovan <kaupiovan@gmail.com>
2024-01-25 05:36:40 -0300

    .

---
+++ Exercicios/S4Ex004/Program.cs
@@ -0,0 +1,15 @@
+﻿var calc = new Calcular();
+
+int potencia = 10;
+potencia = calc.AumentarPotenciaVelocidade(potencia, out double velocidade);
+Console.WriteLine($"Potencia = {potencia}\nVelocidade = {velocidade}");
+
+public class Calcular 
+{
+    public int AumentarPotenciaVelocidade(int potencia, out double velocidade)
+    {
+        int novaPotencia = potencia + 7;
+        velocidade = potencia * 1.75;
+        return novaPotencia;
+    }
+}
