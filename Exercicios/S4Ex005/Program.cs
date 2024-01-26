9197c7d98f46277d7f63355061ae6329a44dfe01
Kauê Piovan <kaupiovan@gmail.com>
2024-01-26 02:06:21 -0300

    .

---
+++ Exercicios/S4Ex005/Program.cs
@@ -0,0 +1,13 @@
+﻿ExibirInfo(modelo: "fusca", montadora: "volkswagen", marca: "volkswagen", potencia: 90);
+ExibirInfo(modelo: "fusca", montadora: "volkswagen", marca: "volkswagen", potencia: 90, ano: 1940);
+
+static void ExibirInfo(string modelo, string montadora, string marca, int potencia, int ano = 0)
+{
+    Console.WriteLine(
+        $"\nModelo: {modelo}\n"+
+        $"Montadora: {montadora}\n"+
+        $"Marca: {marca}\n"+
+        $"Potencia: {potencia}\n"+
+        $"Ano: {ano}\n"
+        );
+}
