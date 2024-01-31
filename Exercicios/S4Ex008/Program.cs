0f50fbdb6f23f0736ed04a711007a5d37098a388
Kauê Piovan <kaupiovan@gmail.com>
2024-01-31 01:42:41 -0300

    .

---
+++ Exercicios/S4Ex008/Program.cs
@@ -0,0 +1,44 @@
+﻿var cliente = new Cliente("joao", "joao@email.com", 16);
+
+Console.WriteLine("Sem passar o argumento de idade\n");
+cliente.ExibirInfo("joao", "joao@email.com");
+Console.WriteLine("Passando o argumento de idade\n");
+cliente.ExibirInfo("joao", "joao@email.com", 35);
+
+public struct Cliente
+{
+    public string Nome;
+    public string Email;
+
+    private int idade;
+    public int Idade
+    {
+        get { return idade; }
+        set
+        {
+            if (value <= 18)
+            {
+                idade = 18;
+                Console.WriteLine("Idade menor que 18");
+            }
+            else
+            {
+                idade = value;
+                Console.WriteLine("Idade maior que 18");
+            }
+        }
+    }
+
+    public Cliente(string nome, string email, int idade)
+    {
+        Nome = nome;
+        Email = email;
+        Idade = idade;
+    }
+
+    public void ExibirInfo(string nome, string email, int idade = 18)
+    {
+        Console.WriteLine($"Nome: {nome}\nEmail: {email}\nIdade: {idade}");
+    }
+
+}
